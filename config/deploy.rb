409ed92219168aa42f2b39c873ca280b2a5fdd5d
Tim Zuev <tim.zuev@gmail.com>
2016-08-25 13:33:48 +0300

    fixed wrond dir in deploy code

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 server '46.101.158.134', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:denker/lifetracker.git'
-set :application,     'LifeTracker'
+set :application,     'lifetracker'
 set :user,            'denker'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
@@ -12,7 +12,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/deploy/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
